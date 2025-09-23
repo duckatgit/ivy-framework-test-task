82c14c4d18fbdfeca4f2be7f5bef45b6f0bfbfd8
Sandeep Deorari <dev1@7CDEVSL.onmicrosoft.com>
2025-09-23 13:21:48 +0530

    first commit

---
+++ Apps/HelloWorldApp.cs
@@ -0,0 +1,16 @@
+﻿
+
+[App(icon: Icons.Shell, title: "Hello World")]
+public class HelloWorldApp : ViewBase
+{
+    public override object? Build()
+    {
+        return Layout.Center()
+               | new Card(
+                   Layout.Vertical().Gap(6).Padding(2)
+                   | Text.H2("Hello World!")
+                   | Text.Block("This is a simple Hello World app.")
+                 )
+                 .Width(Size.Units(120).Max(500));
+    }
+}
